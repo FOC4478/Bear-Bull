b0bcfdcd319674430ec594daa738f78ef8a14cd9
innocent okekem <iokekem@gmail.com>
2025-07-03 04:09:22 -0700

    initial commit - edited some files

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const hamburger = document.getElementById('hamburger');
     hamburger.classList.toggle('open');
     navLinks.classList.toggle('active');
   });
+
+
+  
+  const cards = document.querySelectorAll('.testimonial-card');
+  let index = 0;
+
+  function rotateTestimonials() {
+    cards.forEach(card => card.classList.remove('active'));
+    cards[index].classList.add('active');
+    index = (index + 1) % cards.length;
+  }
+
+  rotateTestimonials(); // Show first on load
+  setInterval(rotateTestimonials, 4000); // Change every 4s
